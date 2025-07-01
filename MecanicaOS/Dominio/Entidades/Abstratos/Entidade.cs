1b35509b868e6943507fd02a0d3b9899ba032c86
Pedro <pmfrp@hotmail.com>
2025-06-30 22:17:24 -0300

    aplicando metodos de comparacao na entidade abstrata

---
+++ MecanicaOS/Dominio/Entidades/Abstratos/Entidade.cs
@@ -0,0 +1,23 @@
+﻿namespace Dominio.Entidades.Abstratos
+{
+    public abstract class Entidade
+    {
+        public Guid Id { get; }
+        public DateTime DataCadastro { get; }
+        public DateTime DataAtualizacao { get; set; }
+
+        protected Entidade()
+        {
+            Id = Guid.NewGuid();
+            DataCadastro = DateTime.Now;
+        }
+
+        public static bool operator ==(Entidade e1, Entidade e2){
+            return e1.Id.Equals(e2.Id);
+        }
+
+        public static bool operator !=(Entidade e1, Entidade e2){
+            return !(e1 == e2);
+        }
+    }
+}
