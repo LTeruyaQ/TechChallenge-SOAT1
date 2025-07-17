a75069f0fdb8d9c8a06f36aa2ff217daddaf5b8f
Pedro <pmfrp@hotmail.com>
2025-07-16 21:31:12 -0300

    testes existentes passando

---
+++ test/MecanicaOSTests/Fixtures/BaseTestFixture.cs
@@ -0,0 +1,48 @@
+using Aplicacao.Mapeamentos;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace MecanicaOSTests.Fixtures
+{
+    public abstract class BaseTestFixture<T> where T : class
+    {
+        protected readonly Mock<ILogger<T>> LoggerMock;
+        protected readonly IMapper Mapper;
+
+        protected BaseTestFixture()
+        {
+            LoggerMock = new Mock<ILogger<T>>();
+            
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddMaps(new[] {
+                    typeof(ClienteProfile),
+                    typeof(EstoqueProfile),
+                    typeof(ServicoProfile),
+                    typeof(UsuarioProfile),
+                    typeof(VeiculoProfile)
+                });
+            });
+            
+            Mapper = configuration.CreateMapper();
+        }
+
+        protected Mock<T> CreateServiceMock<T>() where T : class
+        {
+            return new Mock<T>();
+        }
+
+        protected void SetupLogger<T>(Mock<ILogger<T>> loggerMock, LogLevel expectedLogLevel, string expectedMessage)
+        {
+            loggerMock.Verify(
+                x => x.Log(
+                    expectedLogLevel,
+                    It.IsAny<EventId>(),
+                    It.Is<It.IsAnyType>((v, t) => v.ToString().Contains(expectedMessage)),
+                    It.IsAny<Exception>(),
+                    It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+                Times.Once);
+        }
+    }
+}
---
+++ test/MecanicaOSTests/Fixtures/UsuarioFixture.cs
@@ -0,0 +1,59 @@
+using Aplicacao.DTOs.Requests.Usuario;
+using Dominio.Entidades;
+using Dominio.Enumeradores;
+
+namespace MecanicaOSTests.Fixtures
+{
+    public static class UsuarioFixture
+    {
+        public static CadastrarUsuarioRequest CriarCadastrarUsuarioRequestValido()
+        {
+            return new CadastrarUsuarioRequest
+            {
+                Email = "novo.usuario@teste.com",
+                Senha = "Senha@123",
+                TipoUsuario = TipoUsuario.Cliente,
+                Documento = "12345678901",
+                RecebeAlertaEstoque = true
+            };
+        }
+
+        public static AtualizarUsuarioRequest CriarAtualizarUsuarioRequestValido()
+        {
+            return new AtualizarUsuarioRequest
+            {
+                Email = "usuario.atualizado@teste.com",
+                TipoUsuario = TipoUsuario.Cliente,
+                Documento = "12345678901",
+                RecebeAlertaEstoque = true
+            };
+        }
+
+        public static Usuario CriarUsuarioValido()
+        {
+            return new Usuario
+            {
+                Id = Guid.NewGuid(),
+                Email = "usuario@teste.com",
+                Senha = "hash123",
+                Ativo = true,
+                TipoUsuario = TipoUsuario.Cliente,
+                ClienteId = Guid.NewGuid(),
+                RecebeAlertaEstoque = true
+            };
+        }
+
+        public static Usuario CriarUsuarioAdmin()
+        {
+            return new Usuario
+            {
+                Id = Guid.NewGuid(),
+                Email = "admin@teste.com",
+                Senha = "hash123",
+                Ativo = true,
+                RecebeAlertaEstoque = false,
+                TipoUsuario = TipoUsuario.Admin
+            };
+        }
+    }
+}
