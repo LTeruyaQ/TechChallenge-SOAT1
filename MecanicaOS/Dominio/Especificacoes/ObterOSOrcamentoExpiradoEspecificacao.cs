fefa0b5bbc99be75f12de01f9aa8be72cb6426f5
thalita <thalitafeitosadsilva@gmail.com>
2025-07-21 16:18:56 -0300

    Ajuste Especificacao

--- MecanicaOS/Dominio/Especificacoes/ObterOSOrcamentoExpiradoEspecificacao.cs
+++ MecanicaOS/Dominio/Especificacoes/ObterOSOrcamentoExpiradoEspecificacao.cs
@@ -1,20 +1,19 @@
 ﻿using Dominio.Entidades;
 using Dominio.Enumeradores;
 using Dominio.Especificacoes.Base;
-using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
 namespace Dominio.Especificacoes;
 
 public class ObterOSOrcamentoExpiradoEspecificacao : EspecificacaoBase<OrdemServico>
 {
+    public ObterOSOrcamentoExpiradoEspecificacao()
+    {
+        AdicionarInclusao(os => os.InsumosOS, io => io.Estoque);
+    }
+
     public override Expression<Func<OrdemServico, bool>> Expressao =>
         o => o.Status == StatusOrdemServico.AguardandoAprovação &&
              o.DataEnvioOrcamento.HasValue &&
              o.DataEnvioOrcamento.Value.AddDays(3) <= DateTime.UtcNow;
-
-    public override List<Func<IQueryable<OrdemServico>, IQueryable<OrdemServico>>> Inclusoes =>
-    [
-        o => o.Include(o => o.InsumosOS).ThenInclude(ios => ios.Estoque)
-    ];
 }
