243014fd8899c27935e32140a2562cecb0f481bf
thalita <thalitafeitosadsilva@gmail.com>
2025-09-24 18:29:41 -0300

    k8s

---
+++ MecanicaOS/API/RecurringJobs.cs
@@ -0,0 +1,38 @@
+﻿using API.Jobs;
+using Hangfire;
+
+namespace API
+{
+    public class RecurringJobs
+    {
+        private readonly IRecurringJobManager _recurringJobManager;
+
+        public RecurringJobs(IRecurringJobManager recurringJobManager)
+        {
+            _recurringJobManager = recurringJobManager;
+        }
+
+        public void ScheduleJobs()
+        {
+            _recurringJobManager.AddOrUpdate<VerificarEstoqueJob>(
+                recurringJobId: "verificar-estoque",
+                methodCall: job => job.ExecutarAsync(),
+                cronExpression: Cron.Hourly(),
+                options: new RecurringJobOptions
+                {
+                    TimeZone = TimeZoneInfo.Local
+                }
+            );
+
+            _recurringJobManager.AddOrUpdate<VerificarOrcamentoExpiradoJob>(
+                recurringJobId: "verificar-orcamento-expirado",
+                methodCall: job => job.ExecutarAsync(),
+                cronExpression: Cron.Hourly(),
+                options: new RecurringJobOptions
+                {
+                    TimeZone = TimeZoneInfo.Local
+                }
+            );
+        }
+    }
+}
