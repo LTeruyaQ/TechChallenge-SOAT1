a783e7d52e40eaff903bf8cb91d8cce3ef2402fb
Leandro Teruya de Queiroz <58377166+LTeruyaQ@users.noreply.github.com>
2025-10-08 22:59:50 -0300

    Update CadastrarVariosAlertasHandler.cs

--- MecanicaOS/Core/UseCases/AlertasEstoque/CadastrarVariosAlertas/CadastrarVariosAlertasHandler.cs
+++ MecanicaOS/Core/UseCases/AlertasEstoque/CadastrarVariosAlertas/CadastrarVariosAlertasHandler.cs
@@ -1,24 +1,24 @@
-using Core.DTOs.UseCases.AlertaEstoque;
 using Core.Entidades;
 using Core.Interfaces.Gateways;
 using Core.Interfaces.Handlers.AlertasEstoque;
+using Core.UseCases.Abstrato;
 
 namespace Core.UseCases.AlertasEstoque.CadastrarVariosAlertas
 {
-    public class CadastrarVariosAlertasHandler : ICadastrarVariosAlertasHandler
+    public class CadastrarVariosAlertasHandler : UseCasesHandlerAbstrato<CadastrarVariosAlertasHandler>, ICadastrarVariosAlertasHandler
     {
         private readonly IAlertaEstoqueGateway _alertaEstoqueGateway;
 
-        public CadastrarVariosAlertasHandler(IAlertaEstoqueGateway alertaEstoqueGateway)
+        public CadastrarVariosAlertasHandler(
+            IAlertaEstoqueGateway alertaEstoqueGateway,
+            ILogGateway<CadastrarVariosAlertasHandler> logServicoGateway,
+            IUnidadeDeTrabalhoGateway udtGateway,
+            IUsuarioLogadoServicoGateway usuarioLogadoServicoGateway)
+            : base(logServicoGateway, udtGateway, usuarioLogadoServicoGateway)
         {
             _alertaEstoqueGateway = alertaEstoqueGateway ?? throw new ArgumentNullException(nameof(alertaEstoqueGateway));
         }
-
-        public async Task Handle(IEnumerable<CadastrarAlertaEstoqueUseCaseDto> alertas)
-        {
-            if (alertas == null || !alertas.Any())
-                return;
-
+        
             // Handler (Core) cria as entidades a partir dos DTOs
             var entidades = alertas.Select(dto => new AlertaEstoque
             {
@@ -27,6 +27,8 @@ namespace Core.UseCases.AlertasEstoque.CadastrarVariosAlertas
             });
 
             await _alertaEstoqueGateway.CadastrarVariosAsync(entidades);
+            
+            await Commit();
         }
     }
 }
